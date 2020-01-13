7de4a9c57e6c66c674ebe8c9b96e7b80f73942ae
Roman <student.rv86@gmail.com>
2020-01-13 16:14:48 +0200

    Fixed task4.sql file

--- src/main/resources/task4.sql
+++ src/main/resources/task4.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS new_person (
+CREATE TABLE IF NOT EXISTS person (
     id INT,
     email VARCHAR(255)
 );
 
-TRUNCATE TABLE new_person;
+TRUNCATE TABLE person;
 
-INSERT INTO new_person (id, email) VALUES (1, 'a@b.com');
-INSERT INTO new_person (id, email) VALUES (2, 'c@d.com');
-INSERT INTO new_person (id, email) VALUES (3, 'a@b.com');
+INSERT INTO person (id, email) VALUES (1, 'a@b.com');
+INSERT INTO person (id, email) VALUES (2, 'c@d.com');
+INSERT INTO person (id, email) VALUES (3, 'a@b.com');
 
-SELECT email FROM new_person GROUP BY email
+SELECT email FROM person GROUP BY email
     HAVING COUNT(email) > 1;
